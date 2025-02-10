991ca4137126037ff115e64f0f538cba67f811b5
Pascal Behmenburg <pascalbehmenburg@posteo.net>
2025-02-10 20:19:01 +0100

    styling duplicate group / file view

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,15 @@ import { useState } from "react";
 import { Button } from "./components/ui/button";
 import "./index.css";
 
+const File = ({ path }: { path: string }) => {
+  return (
+    <div className="flex flex-row items-center gap-1 truncate text-pretty">
+      <FileIcon className="size-5" />
+      {path}
+    </div>
+  );
+};
+
 const DuplicateEntry = ({
   hash,
   paths,
@@ -70,9 +79,9 @@ function App() {
   let counter = 1;
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <main className="size-full min-w-screen min-h-screen bg-background text-text">
+      <main className="size-full min-w-screen min-h-screen bg-background text-text antialiased">
         <div className="flex flex-row h-full">
-          <div className="flex flex-col w-full min-h-screen border border-border">
+          <div className="flex flex-col md:w-6/12 min-h-screen border border-border">
             <h1 className="text-4xl text-center py-2 border-b border-border">
               Dedupe
             </h1>
@@ -92,7 +101,10 @@ function App() {
               Index Files
             </Button>
           </div>
-          {/* <div className="flex-col bg-background/50 w-7/12 rounded-lg"></div> */}
+          <div className="flex-col md:w-6/12 mx-1 my-1 pt-1 border-border border min-h-screen">
+            <div className="flex flex-row h-2/12">Filename</div>
+            <div className="flex flex-row bg-green-500">asdsad</div>
+          </div>
         </div>
       </main>
     </ThemeProvider>
